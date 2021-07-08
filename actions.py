57f5470ec5d9b705f99bc4a5e6f516574a2012bc
kevioconnor <77639483+kevioconnor@users.noreply.github.com>
2021-07-08 20:09:31 +0100

    Tiles, map and engine refactor
    Rest of Part 2

--- actions.py
+++ actions.py
@@ -1,8 +1,25 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from engine import Engine
+    from entity import Entity
+
 class Action:
-    pass
+    def perform(self, engine: Engine, entity: Entity) -> None:
+        """
+        Perform action with objects needed to determine scope
+
+        engine is the scope, entity is the object performing action
+
+        Must be overridden by Action subclasses
+        """
+
+        raise NotImplementedError()
 
 class EscapeAction(Action):
-    pass
+    def perform(self, engine: Engine, entity: Entity) -> None:
+        raise SystemExit()
 
 class MovementAction(Action):
     def __init__(self, dx : int, dy : int) -> None:
@@ -10,3 +27,14 @@ class MovementAction(Action):
 
         self.dx = dx
         self.dy = dy
+
+    def perform(self, engine: Engine, entity: Entity) -> None:
+        dest_x = entity.x + self.dx
+        dest_y = entity.y + self.dy
+
+        if not engine.game_map.in_bounds(dest_x, dest_y):
+            return # Destination out of bounds
+        if not engine.game_map.tiles["walkable"][dest_x, dest_y]:
+            return # Destination blocked by tile
+
+        entity.move(self.dx, self.dy)
