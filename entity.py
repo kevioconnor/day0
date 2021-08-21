6a72bf55dba1021850b810e647c87cb53ef86763
kevioconnor <77639483+kevioconnor@users.noreply.github.com>
2021-08-21 21:29:44 +0100

    pre-LevelUpEventHandler
    Late part 11

--- entity.py
+++ entity.py
@@ -11,6 +11,7 @@ if TYPE_CHECKING:
     from components.consumable import Consumable
     from components.fighter import Fighter
     from components.inventory import Inventory
+    from components.level import Level
     from game_map import GameMap
 
 T = TypeVar("T", bound="Entity")
@@ -69,7 +70,7 @@ class Entity:
 
 class Actor(Entity):
     def __init__(self, *, x: int = 0, y: int = 0, char: str = "?", color: Tuple[int, int, int] = (255, 255, 255), 
-    name: str = "<Unnamed>", ai_cls: Type[BaseAI], fighter: Fighter, inventory: Inventory):
+    name: str = "<Unnamed>", ai_cls: Type[BaseAI], fighter: Fighter, inventory: Inventory, level: Level):
         super().__init__(x=x, y=y, char=char, color=color, name=name, blocks_movement=True, render_order=RenderOrder.ACTOR)
 
         self.ai: Optional[BaseAI] = ai_cls(self)
@@ -77,6 +78,8 @@ class Actor(Entity):
         self.fighter.parent = self
         self.inventory = inventory
         self.inventory.parent = self
+        self.level = level
+        self.level.parent = self
 
     @property
     def is_alive(self) -> bool:
--- entity_factories.py
+++ entity_factories.py
@@ -1,16 +1,18 @@
+from components import level
 from components.ai import HostileEnemy
 from components import consumable
 from components.fighter import Fighter
 from components.inventory import Inventory
+from components.level import Level
 from entity import Actor, Item
 
 player = Actor(char="@", color=(255, 255, 255), name="Player", ai_cls=HostileEnemy, 
-        fighter=Fighter(hp=30, defence=2, attack=5), inventory=Inventory(capacity=26))
+        fighter=Fighter(hp=30, defence=2, attack=5), inventory=Inventory(capacity=26), level=Level(level_up_base=200))
 
 caveman = Actor(char="c", color=(255, 242, 161), name="Caveman", ai_cls=HostileEnemy,
-        fighter=Fighter(hp=10, defence=0, attack=3), inventory=Inventory(capacity=26))
+        fighter=Fighter(hp=10, defence=0, attack=3), inventory=Inventory(capacity=0), level=Level(xp_given=30))
 savage = Actor(char="S", color=(255, 220, 0), name="Savage", ai_cls=HostileEnemy,
-        fighter=Fighter(hp=20, defence=1, attack=4), inventory=Inventory(capacity=26))
+        fighter=Fighter(hp=20, defence=1, attack=4), inventory=Inventory(capacity=0), level=Level(xp_given=75))
 
 cavemoss = Item(char="m", color=(15, 215, 55), name="Cave Moss", consumable=consumable.HealingConsumable(amount=3))
 rock = Item(char="r", color=(200, 200, 200), name="Rock", consumable=consumable.RangedDamageConsumable(damage=5, max_range=5))
